30033656cc51d846be00b550c4cd22403d5bf8a3
Allison McKee <am893120@ohio.edu>
2023-02-15 17:08:19 -0500

    complete

--- word-count.cc
+++ word-count.cc
@@ -8,11 +8,59 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <cstring>
 using namespace std;
 
 
 int main(int argc, char const *argv[]) {
+    
+    // initializations
+    char paragraph[500], *token;
+    double word_count = 0;
+    double sentence_count = 0;
+    double average;
 
-    /*add code*/
+    // enter paragraph
+    cout << "Enter a paragraph..." << endl;
+    cin.getline(paragraph, 500);
+    
+    // making spaces indicate new words
+    token = strtok(paragraph, " ");
+    while (NULL != token)
+    {
+        word_count++;
+        token = strtok(NULL, " ");
+    }
+    
+    // making periods, exclamations, and question marks indicate new sentences
+    for (int i = 0; i <= 500; i++)
+    {
+        if (paragraph[i] == '.' || paragraph[i] == '!' || paragraph[i] == '?')
+        {
+            sentence_count++;
+        }
+    }
+
+    // output
+    cout << "@@@" << endl;
+    cout << "Word count: " << word_count << endl;
+    cout << "Sentence count: " << sentence_count << endl;
+
+    // average and error messages
+    cout << fixed << setprecision(1);
+    average = word_count / sentence_count;
+    if (word_count == 0)
+    {
+        cout << "You did not enter any text!" << endl;
+    }
+    else if (sentence_count == 0)
+    {
+        cout << "You did not enter any sentences!" << endl;
+    }
+    else
+    {
+        cout << "Average words per sentence: " << average << endl;
+    }
+
     return 0;
-} /// main
+} 
